3a8d9dcc48c5219095a0e75931d2da6caa15d19e
MacBook <>
2020-07-01 20:18:28 +0800

    no message

--- Sources/App/Routes/routes.swift
+++ Sources/App/Routes/routes.swift
@@ -28,4 +28,68 @@ func routes(_ app: Application) throws {
     }
 
 
+    
+    try webServer(app)
+    
+//    try webClient(app)
+    
+    
 }
+
+
+
+
+//ws 服务器
+func webServer(_ app: Application) throws{
+    app.webSocket("echo") { (req, ws) in
+        print(ws)
+        
+        ws.pingInterval = TimeAmount.seconds(5)
+        
+        ws.send("服务器连接成功！")
+
+        ws.onText { (ws, text) in
+            // 这个方法接收的是字符串
+            print(text)
+            ws.send("服务器接受到的消息："+text)
+        }
+
+        ws.onBinary { ws, binary in
+            // 这个方法接收二进制数组。
+            print(binary)
+        }
+
+        ws.onPing { (ws) in
+            print("onPong")
+//            ws.send("onPong")
+        }
+
+        ws.onPong { (ws) in
+            print("onPong")
+//            ws.send("onPing")
+        }
+
+//        ws.close(promise: nil)
+    }
+}
+
+
+//ws 客户端
+func webClient(_ app: Application) throws{
+    let eventLoop: EventLoopGroup! = MultiThreadedEventLoopGroup(numberOfThreads: 5)
+    let promise = eventLoop.next().makePromise(of: Void.self)
+    
+    WebSocket.connect(to: "ws://127.0.0.1:8080", on: eventLoop) { ws in
+        // Connected WebSocket.
+        print(ws)
+        print("webClient")
+        
+        ws.onText { (ws, text) in
+            // 这个方法接收的是字符串
+            print(text)
+            ws.send("服务器接受到的消息："+text)
+        }
+    }.cascadeFailure(to: promise)
+}
+
+
